b3594c2c6dc89d2affd1b7ed2258951cbbba1157
Ruslan Gainutdinov <contact@ruslan.org>
2023-04-27 22:56:27 +0300

    fix: separate file to exclude Webpack plugin from web bundle

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,8 @@
-/**
- * Parse spreadsheet and generate structured content.
- * If no fileName is provided, will return parsed object.
- *
- * @param spreadsheetId Spreadsheet ID
- * @param sheetName Sheet name, optional, if not provided will use first sheet
- * @param sheetId Sheet ID, optional, if not provided will use first sheet
- * @param fs Filesystem module (i.e. require('fs'))
- * @param fileName Source code file to write.
- * @param variable Variable name
- * @returns Promise with parsed object
- */
-export function parse(
-  spreadsheetId: string,
-  sheetName?: string,
-  sheetId?: string,
-  fs: unknown,
-  fileName: string | undefined,
-  variable: string | undefined
-): Promise<Record<string, unknown>>;
+import type { parse, createStructuredObject } from "./parse.d.ts";
+
+declare module "generate-content-spreadsheet/parse" {
+  export { parse, createStructuredObject };
+}
 
 /**
  * Webpack plugin to parse spreadsheet.
@@ -26,4 +11,8 @@ export function parse(
  * */
 export class GenerateContentSpreadsheetPlugin {
   constructor(fileName: string, variable: string, spreadsheetId: string, sheetName?: string, sheetId?: string);
+}
+
+declare module "generate-content-spreadsheet" {
+  export { parse, createStructuredObject, GenerateContentSpreadsheetPlugin };
 }
---
+++ parse.d.ts
@@ -0,0 +1,25 @@
+/**
+ * Parse spreadsheet and generate structured content.
+ * If no fileName is provided, will return parsed object.
+ *
+ * @param spreadsheetId Spreadsheet ID
+ * @param sheetName Sheet name, optional, if not provided will use first sheet
+ * @param sheetId Sheet ID, optional, if not provided will use first sheet
+ * @param fs Filesystem module (i.e. require('fs'))
+ * @param fileName Source code file to write.
+ * @param variable Variable name
+ * @returns Promise with parsed object
+ */
+export function parse(
+  spreadsheetId: string,
+  sheetName?: string,
+  sheetId?: string,
+  fs?: unknown,
+  fileName?: string,
+  variable?: string
+): Promise<Record<string, unknown>>;
+
+/**
+ * From key, value array, creates a structured object, with support for array notation.
+ */
+export function createStructuredObject(data: { key: string; value: string }[]): Record<string, unknown>;
